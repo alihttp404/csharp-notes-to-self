3417c722cff77f5ef8129c3c64d305c1973a7a93
Ali <122698924+alihttp404@users.noreply.github.com>
2023-08-26 18:38:33 +0400

    .

--- Input Output/Input Output/Program.cs
+++ Input Output/Input Output/Program.cs
@@ -5,7 +5,7 @@ Console.Write("Hello World "); // no new line at the end of string
 Console.Clear(); // clears the console
 
 // Input
-
+ 
 Console.ReadLine(); // reads a line of text from the console
 Console.Read(); // reads a single character from the console
 Console.ReadKey(); // reads a key press, including special keys like function keys.
