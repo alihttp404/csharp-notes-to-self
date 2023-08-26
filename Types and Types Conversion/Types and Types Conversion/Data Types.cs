32612571f1f6bfd7af31faae62e906d453a905fe
Ali Mammadov <ali.mammadov@easlearn.com>
2023-08-26 18:23:12 +0400

    Data Types, Type Conversions, Type Safety, Default Values, Null, Anonymous data types

---
+++ Types and Types Conversion/Types and Types Conversion/Data Types.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types_and_Types_Conversion
+{
+    internal class Data_Types
+    {
+        // Data Types
+
+        // 1. Value Types - Work with value of data. Stored in the stack
+        // Examples - struct, enum, nullabel - value types
+
+        // 2. Reference Types - Work with references to data (addresses of them). Stored in the heap
+        // Examples - class, interface, delegate, array
+    }
+}
---
+++ Types and Types Conversion/Types and Types Conversion/Program.cs
@@ -0,0 +1,19 @@
+﻿using Types_and_Types_Conversion;
+
+// Anonymous data types: var and dynamic
+
+// var
+// type is set during compile time, its type can't be changed afterwards, meaning whatever type its initial value is, all modifications must be of that type
+
+// Example: (error)
+//var name = "Name";
+//name = 15; // Error
+
+
+// dynamic
+// type is set during runtime, its type can be changed afterwards, unlike var
+
+// Example: (okay)
+    // dynamic name_2 = "Step";
+    // Console.WriteLine(name_2.ToLower());
+    // name_2 = 15.35;
