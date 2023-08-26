7266b521245c0b08000945c2fa657ef7e5fd486e
Ali Mammadov <ali.mammadov@easlearn.com>
2023-08-26 13:54:03 +0400

    .

---
+++ Output Operator Overloading/Output Operator Overloading/MyClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Output_Operator_Overloading
+{
+    internal class MyClass
+    {
+        public string? _name { get; private set; }
+
+        public MyClass()
+        {
+            _name = null;
+            Console.WriteLine("defualt constructor");
+        }
+
+        public MyClass(string name)
+        {
+            _name = name;
+        }
+
+        public override string ToString() // from base class "object"
+        {
+            return $"Object name: {_name}";
+        }
+    }
+}
---
+++ Output Operator Overloading/Output Operator Overloading/Program.cs
@@ -0,0 +1,5 @@
+﻿using Output_Operator_Overloading;
+
+Console.WriteLine("Hello, World!");
+MyClass myClass = new MyClass("ali");
+Console.WriteLine(myClass);
